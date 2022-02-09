30bec4e7818135512215388f9df4ca3362799fee
Serhii Ostrikov <serostrikov@gmail.com>
2022-02-09 22:31:25 +0200

    added real mode transition

--- makefile
+++ makefile
@@ -5,7 +5,7 @@ ${BUILD_DIR}/entry.bin: ${BUILD_DIR}/entry.o
 	ld -m elf_i386 -Ttext 0x7c00 -o ${BUILD_DIR}/entry.bin ${BUILD_DIR}/entry.o --oformat binary
 
 ${BUILD_DIR}/entry.o: ${SRC_DIR}/entry.s
-	gcc -m16 -c ${SRC_DIR}/entry.s -o ${BUILD_DIR}/entry.o  
+	gcc -m32 -c ${SRC_DIR}/entry.s -o ${BUILD_DIR}/entry.o  
 
 clear:
 	rm ${BUILD_DIR}/*
--- src/entry.s
+++ src/entry.s
@@ -3,35 +3,47 @@
 _start: 
 	jmp main
 
-pr_msg:
-	movb $0x0e, %ah
-	movw $0x7e00, %bx
-while:
-	cmp $0, (%bx)
-	je whileend
-	movb %es:(%bx), %al 
-	int $0x10 
-	inc %bx
-	jmp while
-whileend:
-	ret
+gdt_start:
+null_desc:
+	.int 0
+	.int 0
+code_desc:
+	.word 0xffff 
+	.word 0x0
+	.byte 0x0 
+	.byte 0b10011010 
+	.byte 0b11001111 
+	.byte 0 
+data_desc:
+	.word 0xffff
+	.word 0x0
+	.byte 0x0 
+	.byte 0b10010010
+	.byte 0b11001111
+	.byte 0x0
+gdt_end:
+
+gdt_desc:
+	.word gdt_end - gdt_start - 1
+	.long gdt_start
+
+CODE_SEG = code_desc - gdt_start
+DATA_SEG = data_desc - gdt_start
+
 main:
-	movw $0, %ax
-	movw %ax, %es
-	movb %dl, (disknum)
-	movb $2, %ah
-	movb $1, %al
-	movb $0, %ch
-	movb $2, %cl
-	movb $0, %dh
-	movb (disknum), %dl
-	movw $0x7e00, %bx
-	int  $0x13
-	call pr_msg		
-	hlt
-disknum:
-	.byte 0x00
+	cli 
+	lgdt (gdt_desc)
+	movl %cr0, %eax
+	or $1, %eax
+	movl %eax, %cr0
+	jmp $0x08, $prot_start
+
+.code32
+prot_start:
+	movb $'A', %al
+	movb $0x0f, %ah
+	movw %ax, (0xb8000) 
+	jmp $CODE_SEG, $prot_start
 
 .= _start + 510
 .word 0xaa55
-.ascii "Hello, disk!"
